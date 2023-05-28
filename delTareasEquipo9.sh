249ab660716254a63f212021f758a5bc1bbd7124
Agustina Barbona <agusmua3110@gmail.com>
2023-05-28 16:37:56 -0300

    Ahora tenemos los tres ejercicios

--- delTareasEquipo9.sh
+++ delTareasEquipo9.sh
@@ -1,14 +1,1 @@
-#!/bin/bash
-
-echo "¿Quieres borrar tareas?"
-echo "Contenido de TareasDiarias.txt:"
 cat TareasDiarias.txt
-
-read -p "Ingrese la tarea que quiera borrar: " tarea
-
-if grep -q "$tarea" TareasDiarias.txt; then
-    sed -i "/$tarea/d" TareasDiarias.txt
-    echo "Tarea  eliminada correctamente de TareasDiarias.txt."
-else
-    echo "La tarea  no existe en TareasDiarias.txt."
-fi
